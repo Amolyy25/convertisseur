d5651a849c30eb009206e0351982ba699406b8d9
Amolyy25 <153266275+Amolyy25@users.noreply.github.com>
2025-06-01 12:46:27 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "javascript/auto",
+      loader: "file-loader",
+    });
+
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig 
